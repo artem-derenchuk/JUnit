b120c126ed60d6659afdeb7fcf1be3ff838af4de
artem-derenchuk <derenchuk2001@mail.ru>
2021-12-06 12:23:39 +0300

    recommit order tests and nested

---
+++ src/test/java/BankAccountNestedTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankAccountNestedTest {
+
+    @Test
+    @DisplayName("Withdraw 500 succesfully")
+    public void testWithDraw() {
+        BankAccount bankAccount = new BankAccount(500, -1000);
+        bankAccount.withdraw(300);
+        assertEquals(200, bankAccount.getBalance());
+    }
+
+    @Test
+    @DisplayName("Deposit 400 succesfully")
+    public void testDeposit() {
+        BankAccount bankAccount = new BankAccount(400, 0);
+        bankAccount.deposit(500);
+        assertEquals(900, bankAccount.getBalance());
+    }
+
+    @Nested
+    class WhenBalanceEqualsZero {
+        @Test
+        @DisplayName("Withdrawing below minimum balance: exception")
+        public void testWithdrawMinimumBalance0() {
+            BankAccount bankAccount = new BankAccount(0, 0);
+            assertThrows(RuntimeException.class, () -> bankAccount.withdraw(500));
+        }
+
+        @Test
+        @DisplayName("Withdrawing and getting a negative balance")
+        public void testWithdrawMinimumBalanceNegative1000() {
+            BankAccount bankAccount = new BankAccount(0, -1000);
+            bankAccount.withdraw(500);
+            assertEquals(-500, bankAccount.getBalance());
+        }
+    }
+}
